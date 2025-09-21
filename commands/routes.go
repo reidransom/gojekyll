23179fecebb13feb18e84f0e99948bc895bcef93
Reid Ransom <reid@reidransom.com>
2025-09-21 04:46:17 -0400

    implement json output for routes

--- commands/routes.go
+++ commands/routes.go
@@ -1,6 +1,7 @@
 package commands
 
 import (
+	"encoding/json"
 	"fmt"
 	"sort"
 
@@ -9,19 +10,38 @@ import (
 
 var routes = app.Command("routes", "Display site permalinks and associated files")
 var dynamicRoutes = routes.Flag("dynamic", "Only show routes to non-static files").Bool()
+var jsonRoutes = routes.Flag("json", "Output routes in JSON format").Bool()
 
 func routesCommand(site *site.Site) error {
-	logger.label("Routes:", "")
-	var urls []string
-	for u, p := range site.Routes {
-		if !*dynamicRoutes || !p.IsStatic() {
-			urls = append(urls, u)
+	if *jsonRoutes {
+		// Create a map for JSON output
+		routeMap := make(map[string]string)
+		for u, p := range site.Routes {
+			if !*dynamicRoutes || !p.IsStatic() {
+				routeMap[u] = p.Source()
+			}
 		}
-	}
-	sort.Strings(urls)
-	for _, u := range urls {
-		filename := site.Routes[u].Source()
-		fmt.Printf("  %s -> %s\n", u, filename)
+		
+		// Output JSON
+		jsonData, err := json.MarshalIndent(routeMap, "", "  ")
+		if err != nil {
+			return err
+		}
+		fmt.Println(string(jsonData))
+	} else {
+		// Original text output
+		logger.label("Routes:", "")
+		var urls []string
+		for u, p := range site.Routes {
+			if !*dynamicRoutes || !p.IsStatic() {
+				urls = append(urls, u)
+			}
+		}
+		sort.Strings(urls)
+		for _, u := range urls {
+			filename := site.Routes[u].Source()
+			fmt.Printf("  %s -> %s\n", u, filename)
+		}
 	}
 	return nil
 }
