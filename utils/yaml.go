e6c359e6b8d6a57b8e43dbbc97760658243b3efb
Reid Ransom <reid@reidransom.com>
2025-10-25 10:06:30 -0400

    support list of objects at root of data file

--- utils/yaml.go
+++ utils/yaml.go
@@ -15,12 +15,54 @@ func UnmarshalYAMLInterface(b []byte, i *interface{}) error {
 		if err != nil {
 			return err
 		}
-		*i = s
+		*i = convertYAMLValue(s)
 	default:
 		if err != nil {
 			return err
 		}
-		*i = m
+		// Check if this is actually an array that was incorrectly parsed as MapSlice
+		// (happens when YAML array is unmarshaled into MapSlice - it creates nil keys)
+		if len(m) > 0 && m[0].Key == nil {
+			var s []interface{}
+			err = yaml.Unmarshal(b, &s)
+			if err != nil {
+				return err
+			}
+			*i = convertYAMLValue(s)
+		} else {
+			*i = convertYAMLValue(m)
+		}
 	}
 	return nil
+}
+
+// convertYAMLValue recursively converts yaml.MapSlice and map[interface{}]interface{}
+// to map[string]interface{} and processes nested structures
+func convertYAMLValue(v interface{}) interface{} {
+	switch v := v.(type) {
+	case yaml.MapSlice:
+		m := make(map[string]interface{}, len(v))
+		for _, item := range v {
+			if key, ok := item.Key.(string); ok {
+				m[key] = convertYAMLValue(item.Value)
+			}
+		}
+		return m
+	case map[interface{}]interface{}:
+		m := make(map[string]interface{}, len(v))
+		for key, value := range v {
+			if keyStr, ok := key.(string); ok {
+				m[keyStr] = convertYAMLValue(value)
+			}
+		}
+		return m
+	case []interface{}:
+		result := make([]interface{}, len(v))
+		for i, item := range v {
+			result[i] = convertYAMLValue(item)
+		}
+		return result
+	default:
+		return v
+	}
 }
