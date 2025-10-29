c415e4862e89a45945313c28315038d4559192d7
Reid Ransom <reid@reidransom.com>
2025-10-29 17:46:51 -0400

    fix test runner

--- commands/run_test.go
+++ commands/run_test.go
@@ -1,12 +1,15 @@
 package commands
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestParseAndRun(t *testing.T) {
+	// Clean up _site directory before test
+	os.RemoveAll("testdata/site/_site")
 	err := ParseAndRun([]string{"build", "-s", "testdata/site", "-q"})
 	require.NoError(t, err)
 }
