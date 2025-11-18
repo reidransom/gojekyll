0c07429c522cccde2eaa6fe8e72e246fd8558db4
Reid Ransom <reid@reidransom.com>
2025-11-18 04:41:24 -0500

    handle common jekyll date formats

--- frontmatter/read.go
+++ frontmatter/read.go
@@ -3,6 +3,7 @@ package frontmatter
 import (
 	"bytes"
 	"regexp"
+	"time"
 
 	"github.com/osteele/gojekyll/utils"
 
@@ -39,6 +40,8 @@ func Read(sourcePtr *[]byte, firstLine *int) (fm FrontMatter, err error) {
 		if err = yaml.Unmarshal(source[match[2]:match[3]], &fm); err != nil {
 			return
 		}
+		// Convert date strings to time.Time
+		convertDates(fm)
 	} else if match := emptyFontMatterMatcher.FindSubmatchIndex(source); match != nil {
 		start = match[1]
 	}
@@ -47,4 +50,50 @@ func Read(sourcePtr *[]byte, firstLine *int) (fm FrontMatter, err error) {
 	}
 	*sourcePtr = source[start:]
 	return
+}
+
+// convertDates recursively converts date strings to time.Time in front matter
+func convertDates(m map[string]interface{}) {
+	for k, v := range m {
+		switch val := v.(type) {
+		case string:
+			// Try to parse common date formats
+			if t, err := parseDate(val); err == nil {
+				m[k] = t
+			}
+		case map[string]interface{}:
+			convertDates(val)
+		case []interface{}:
+			for i, item := range val {
+				if itemMap, ok := item.(map[string]interface{}); ok {
+					convertDates(itemMap)
+				} else if itemStr, ok := item.(string); ok {
+					if t, err := parseDate(itemStr); err == nil {
+						val[i] = t
+					}
+				}
+			}
+		}
+	}
+}
+
+// parseDate attempts to parse a string as a date using common formats
+func parseDate(s string) (time.Time, error) {
+	// Common date formats used in Jekyll
+	formats := []string{
+		time.RFC3339,                     // 2006-01-02T15:04:05Z07:00
+		"2006-01-02 15:04:05-07:00",       // 2025-01-01 01:00:00+00:00
+		"2006-01-02 15:04:05 -07:00",      // With space before timezone
+		"2006-01-02T15:04:05",             // Without timezone
+		"2006-01-02 15:04:05",             // Without timezone, space separator
+		"2006-01-02",                      // Date only
+	}
+	
+	for _, format := range formats {
+		if t, err := time.Parse(format, s); err == nil {
+			return t, nil
+		}
+	}
+	
+	return time.Time{}, &time.ParseError{}
 }
